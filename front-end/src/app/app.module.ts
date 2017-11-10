bf1ae241e25b3be7f0f23f4a67799d9b948694e8
emmadema <emma.demango@gmail.com>
2017-11-10 13:30:27 -0700

    linking bootstrap

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+  
 import  { AppRoutingModule } from './app-routing.module';
 import  { MainFoodRoutingModule } from './mainFood/mainFood-routing.module';
 
@@ -19,7 +20,8 @@ import { MainFoodModule } from './mainFood/mainFood.module';
     AppRoutingModule,
     MainFoodRoutingModule,
     MainFoodModule,
-    HttpModule
+    HttpModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
