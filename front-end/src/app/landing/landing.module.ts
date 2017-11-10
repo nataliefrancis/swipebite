9559e4817263f8301436176052fce2a34e341a59
mfullford <marissafullford@gmail.com>
2017-11-09 17:18:07 -0700

    Styled landing page and onboarding cards

--- front-end/src/app/landing/landing.module.ts
+++ front-end/src/app/landing/landing.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { LoginButtonComponent } from './login-button/login-button.component';
 import { LandingService } from './landing-service.service';
 import { HeaderComponent } from './header/header.component';
+import { FirstCardComponent } from './first-card/first-card.component';
+import { SecondCardComponent } from './second-card/second-card.component';
+import { ThirdCardComponent } from './third-card/third-card.component';
+import { FourthCardComponent } from './fourth-card/fourth-card.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoginButtonComponent, HeaderComponent],
+  declarations: [LoginButtonComponent, HeaderComponent, FirstCardComponent, SecondCardComponent, ThirdCardComponent, FourthCardComponent],
   providers: [LandingService],
-  exports: [ LoginButtonComponent ]
+  exports: [ LoginButtonComponent, HeaderComponent, FirstCardComponent]
 })
 export class LandingModule { }
---
+++ front-end/src/app/landing/third-card/third-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ThirdCardComponent } from './third-card.component';
+
+describe('ThirdCardComponent', () => {
+  let component: ThirdCardComponent;
+  let fixture: ComponentFixture<ThirdCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ThirdCardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ThirdCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
