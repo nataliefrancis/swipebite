706a2bdcb154ee94ac3d1d9a1a8f4462863bd566
mfullford <marissafullford@gmail.com>
2017-11-13 11:32:00 -0700

    Fixes landing landing landing problem

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,18 +5,23 @@ import { LandingComponent } from './landing/landing.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { MainFoodComponent } from './main-food/main-food.component';
 
+import { LandingModule } from './landing/landing.module';
+import { MainFoodModule } from './main-food/main-food.module';
+
+
 const routes: Routes = [
     {
         path: '',
-        redirectTo: '/landing',
-        pathMatch: 'full'
+        redirectTo: 'landing',
+        // loadChildren: './landing/landing.module',
+        pathMatch: 'full',
         // component: LandingComponent
     },
     {
-    	path: 'main',
-        // redirectTo: '/main',
+    	path: 'landing/main',
+        redirectTo: '/main',
         pathMatch: 'full',
-        component: MainFoodComponent
+        // component: MainFoodComponent
     },
     {
         path:'favorites',
