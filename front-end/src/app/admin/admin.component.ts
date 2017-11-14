495906efdb161340996c8067a047998c7cb36451
Courtney Fay <fay.courtney@gmail.com>
2017-11-14 10:55:38 -0700

    creates routes for delete functions which are not currently working in the database

--- front-end/src/app/admin/admin.component.ts
+++ front-end/src/app/admin/admin.component.ts
@@ -43,6 +43,21 @@ export class AdminComponent implements OnInit {
   	})
   }
 
+  deleteOneRestaurant(restaurantId) {
+  	console.log('restaurantId at admin component:' + restaurantId);
+  	this.apiService.destroyOneRestaurant(restaurantId)
+  	.subscribe(response => {
+  		console.log(response.json());
+  		// this.oneFood = response.json();
+  		console.log('all the restaurants in the array:');
+  		console.log(this.restaurants);
+  		console.log('restaurantId after returning from the DB:' + restaurantId);
+  		let restaurantIndex = this.restaurants.indexOf(restaurantId);
+  		console.log('restaurant index: ' + restaurantIndex);
+  		this.restaurants.splice(restaurantIndex, 1);  	
+  	})
+  }
+
   // FOOD CALLS //
 
   grabsAllFoods() {
@@ -80,10 +95,12 @@ export class AdminComponent implements OnInit {
   	this.apiService.destroyOneFood(foodId)
   	.subscribe(response => {
   		console.log(response.json());
+  		// this.oneFood = response.json();
+  		console.log('all the food in the array:');
   		console.log(this.foods);
   		console.log('foodId after returning from the DB:' + foodId);
   		let foodIndex = this.foods.indexOf(foodId);
-  		console.log(foodIndex);
+  		console.log('food index: ' + foodIndex);
   		this.foods.splice(foodIndex, 1);  	
   	})
   }
@@ -129,6 +146,21 @@ export class AdminComponent implements OnInit {
   	.subscribe(response => {
   		console.log(response.json());
   		this.oneUser = response.json();
+  	})
+  }
+
+  deleteOneUser(userId) {
+  	console.log('userId at admin component:' + userId);
+  	this.apiService.destroyOneUser(userId)
+  	.subscribe(response => {
+  		console.log(response.json());
+  		// this.oneFood = response.json();
+  		console.log('all the users in the array:');
+  		console.log(this.users);
+  		console.log('userId after returning from the DB:' + userId);
+  		let userIndex = this.users.indexOf(userId);
+  		console.log('user index: ' + userIndex);
+  		this.users.splice(userIndex, 1);  	
   	})
   }
 
