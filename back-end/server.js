03f680147feeec480b2e78364fce7bc992e752f2
Courtney Fay <fay.courtney@gmail.com>
2017-12-12 16:04:23 -0700

    more of the https stuff on heroku

--- back-end/server.js
+++ back-end/server.js
@@ -40,7 +40,7 @@ if(!process.env.DYNO) {
 		// if(req.header "x-forwarded-proto" !== "https") 
 		// 	res.redirect(`https://${req.header('host')}${req.url}`);
 		// else
-		// 	next()
+		next();
 	});
 }
 
